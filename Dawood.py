34405f408ae837258cd44ae801c5d1c65056efce
Dawood-Adams <adamsdawood363@gmail.com>
2026-02-18 11:06:46 +0300

    Create Dawood.py

---
+++ Dawood.py
@@ -0,0 +1,10 @@
+Name = "Dawood"
+Favourite_sports = "Football"
+Grade = "A-"
+Hobby = "Reading Novels"
+Course = "Electrical Engineering"
+print(Name)
+print(Favourite_sports)
+print(Grade)
+print(Hobby)
+print(Course)
