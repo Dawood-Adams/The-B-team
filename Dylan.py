8fc05c0beb02ba701e90b04d7072c58cdb6cc03f
DylanMK <dkinyua979@gmail.com>
2026-02-20 08:42:55 +0300

    Update Dylan.py

--- Dylan.py
+++ Dylan.py
@@ -1,5 +1,11 @@
-Name:Dylan Muriuki
-Grade:A-
-Hobbies:Writing short narratives
-Favourite sport:Swimming
-Course:Electrical_engineering
+Name:"Dylan Muriuki"
+Grade:"A-"
+Hobby:"writing short narratives"
+Favourite_sport:"swimming"
+Course:"Electrical and electronics engineering"
+
+print(f"My name is {Name}")
+print(f"I got an {Grade} in my KCSE")
+print(f"I enjoy {Hobby} for fun")
+print(f"My favourite sport is {Favourite_sport}")
+print(f"My course of choice is {Course}")
